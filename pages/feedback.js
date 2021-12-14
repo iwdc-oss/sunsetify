be55475ddc401410070b5e95d0b84d6fa2990592
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-12-14 20:12:16 +0700

    feat: add feedback page

---
+++ pages/feedback.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import { FeedbackForm } from '@/src/components/FeedbackForm'
+
+export default function Feedback() {
+  return (
+    <div className='py-10 px-3'>
+      <div className='mb-12'>
+        <h1 className='text-center text-6xl font-poppins'>Sunsetify</h1>
+        <h2 className='text-center font-poppins italic mt-1'>
+          <span className='font-bold'>from</span> <span className='underline'>IPB Web Development Community</span>
+        </h2>
+        <h2 className='mt-6 text-center text-lg font-poppins'>
+          "Berapakah kadar <span className='text-yellow-600 font-medium'>Senja</span> Anda pada semester ini?"
+        </h2>
+      </div>
+      <div
+        className='overflow-auto'
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <FeedbackForm />
+      </div>
+    </div>
+  )
+}
